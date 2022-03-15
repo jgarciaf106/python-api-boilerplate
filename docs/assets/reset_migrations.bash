1d4d3974b31d9868dd6a7a438f9fde2976f94c67
Andres Garcia <joseandresgarciafernandez@dev.azure.com>
2022-03-15 16:41:00 -0600

    API initial Commit

--- docs/assets/reset_migrations.bash
+++ docs/assets/reset_migrations.bash
@@ -1,7 +1,7 @@
 rm -R -f ./migrations &&
 pipenv run init &&
-psql -U gitpod -c 'DROP DATABASE example;' || true &&
-psql -U gitpod -c 'CREATE DATABASE example;' &&
-psql -U gitpod -c 'CREATE EXTENSION unaccent;' -d example &&
+psql -U gitpod -c 'DROP DATABASE inventory;' || true &&
+psql -U gitpod -c 'CREATE DATABASE inventory;' &&
+psql -U gitpod -c 'CREATE EXTENSION unaccent;' -d inventory &&
 pipenv run migrate &&
 pipenv run upgrade
--- src/api/models.py
+++ src/api/models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
+        
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
@@ -13,7 +14,39 @@ class User(db.Model):
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            "username": self.username,
+            "name": self.name,
+            "is_active": self.is_active,
+            # do not serialize the password, its a security breach
+        }
+        
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cat_code = db.Column(db.Integer, unique=True, nullable=False)
+    description = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.cat_code
+
+    def serialize(self):
+        return {
+            "cat_code": self.cat_code,
+            "description": self.description,
+        }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    prod_id = db.Column(db.Integer, unique=True, nullable=False)
+    cat_code = db.Column(db.Integer, db.ForeignKey('category.cat_code'), unique=False, nullable=False)
+    description = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Product %r>' % self.prod_id
+
+    def serialize(self):
+        return {
+            "prod_id": self.prod_id,
+            "prod_id": self.cat_code,
+            "description": self.description,
             # do not serialize the password, its a security breach
         }
