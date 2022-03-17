ca268dc519dbe1724448eba9b0aac6fe0b40da5f
jgarciaf106 <jgarciaf106@gmail.com>
2022-03-17 15:31:26 +0000

    Update Migrations

--- migrations/versions/47398a32117d_.py
+++ migrations/versions/47398a32117d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: c98bed2b2568
+Revision ID: 47398a32117d
 Revises: 
-Create Date: 2022-03-16 22:25:25.076039
+Create Date: 2022-03-17 15:30:50.286347
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c98bed2b2568'
+revision = '47398a32117d'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -38,7 +38,7 @@ def upgrade():
     sa.Column('username', sa.String(length=120), nullable=False),
     sa.Column('name', sa.String(length=120), nullable=False),
     sa.Column('password', sa.String(length=80), nullable=False),
-    sa.Column('is_active', sa.Boolean(), nullable=False),
+    sa.Column('is_admin', sa.Boolean(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
